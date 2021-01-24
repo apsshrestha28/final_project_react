a9b7f9471429d5d99e5ef3e959d90ed3ecc71fb1
Aleena <apsshrestha28>
2021-01-24 14:00:38 -0800

    accepted ride request lists created

--- src/components/DriverProfilePage.js
+++ src/components/DriverProfilePage.js
@@ -52,7 +52,7 @@ class DriverProfilePage extends Component {
     }
     RideRequest.update(actionParms)
       .then((s => {
-        alert('saved');
+        window.location.reload(true);
       }))
   }
 
@@ -60,7 +60,7 @@ class DriverProfilePage extends Component {
    render(){
     return(
       <main className='bgImage'>
-        <h4 style={{textAlign: 'center', color:'black',border:' 1.5px solid black', backgroundColor:'white',fontFamily:'serif'}}>Your Profile Page</h4>   
+        <h3 style={{textAlign: 'center', color:'black',border:' 1.5px solid black', backgroundColor:'white',fontFamily:'serif'}}>Your Profile Page</h3>   
         <div style={{color:'white',marginLeft:'1.5rem'}}>
             <p key={this.state.user.id}> </p>
             <p>Name  = {this.state.user.first_name} {this.state.user.last_name}</p>
@@ -72,7 +72,7 @@ class DriverProfilePage extends Component {
         </div>
 
         <div style={{ padding:'22px'}}>
-          <h6 style={{color:'black'}}><u>Ride Requests Pending List</u></h6>
+          <h5 style={{color:'black'}}><u>Ride Requests Pending List</u></h5>
           <Table striped bordered hover variant="dark">
             <thead  style= {{fontSize:'.8rem'}}>
               <tr>
@@ -87,9 +87,7 @@ class DriverProfilePage extends Component {
               </tr>
             </thead>
             <tbody style= {{fontSize:'.8rem'}}> 
-                   
-              {this.state.ride_requests.map((c, index) => {
-
+              {this.state.ride_requests.filter(a => a.status === '').map((c, index) => {  
                 if(this.state.customers[index]){ 
                   return(
                     <tr key={c.id}>
@@ -99,8 +97,41 @@ class DriverProfilePage extends Component {
                       <td>{this.state.customers[index].address} </td>
                       <td> {c.ride_date}</td>
                       <td> {c.ride_time.substring(11,16)}</td>
-                      <td> <button onClick={() => this.acceptRequest(c.id, 'accept')}>Accept</button></td>
-                      <td> <button onClick={() => this.acceptRequest(c.id , 'decline')}>Decline</button></td>           
+                      <td> <button onClick={() => this.acceptRequest(c.id, 'accept')} className= 'btn btn-success btn-sm'>Accept</button></td>
+                      <td> <button onClick={() => this.acceptRequest(c.id , 'decline')} className= 'btn btn-danger btn-sm'>Decline</button></td>           
+                    </tr>
+                  )
+                }
+
+              })}       
+            </tbody>
+          </Table>                     
+        </div>
+
+        <div style={{ padding:'22px'}}>
+          <h5 style={{color:'black'}}><u>Ride Requests Accepted List</u></h5>
+          <Table striped bordered hover variant="dark">
+            <thead  style= {{fontSize:'.8rem'}}>
+              <tr>
+                <th>S.N</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Address</th>
+                <th>Ride Date</th>
+                <th>Ride Time</th>
+              </tr>
+            </thead>
+            <tbody style= {{fontSize:'.8rem'}}> 
+              {this.state.ride_requests.filter(a => a.status === 'accept').map((c, index) => {  
+                if(this.state.customers[index]){ 
+                  return(
+                    <tr key={c.id}>
+                      <td>{index + 1} </td>
+                      <td>{this.state.customers[index].first_name}</td>
+                      <td>{this.state.customers[index].last_name}</td>
+                      <td>{this.state.customers[index].address} </td>
+                      <td> {c.ride_date}</td>
+                      <td> {c.ride_time.substring(11,16)}</td>        
                     </tr>
                   )
                 }
